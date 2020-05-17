cbf47a715b0b86c0eb992d7b7594622b9a906a84
Victor Espaillat <vicander_99@hotmail.com>
2020-05-17 10:12:32 -0400

    96. Reading CSV files

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-import fs from "fs"
-console.log("Hi there");
+import fs from "fs";
+
+const matches = fs
+    .readFileSync("football.csv", { encoding: "utf-8" })
+    .split("\n")
+    .map((row: string): string[] => row.split(","));
+
+console.log(matches);
