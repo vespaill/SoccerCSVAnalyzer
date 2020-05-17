8002383f5d79328f5799d8b884940d822605b76a
Victor Espaillat <vicander_99@hotmail.com>
2020-05-17 11:31:27 -0400

    106. Describing a row with a tuple

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -2,8 +2,11 @@ import fs from "fs";
 import { dateStringToDate } from "./utils";
 import { MatchResult } from "./MatchResult";
 
+/* Tuple declaration */
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 export class CsvFileReader {
-    data: string[][] = [];
+    data: MatchData[] = [];
     /* Declare class member 'filename' */
     /* automatically initialized with constructor paramter */
     constructor(public filename: string) {}
@@ -12,18 +15,16 @@ export class CsvFileReader {
             .readFileSync(this.filename, { encoding: "utf-8" })
             .split("\n")
             .map((row: string): string[] => row.split(","))
-            .map((row: string[]): any => {
-                return [
-                    dateStringToDate(row[0]),
-                    row[1],
-                    row[2],
-                    parseInt(row[3]),
-                    parseInt(row[4]),
-                    /* Type assertion. Consider this value as one of the
-                       possible values of MatchResult ('H', 'A', 'D')  */
-                    row[5] as MatchResult,
-                    row[6]
-                ];
-            });
+            .map((row: string[]): MatchData => [
+                dateStringToDate(row[0]),
+                row[1],
+                row[2],
+                parseInt(row[3]),
+                parseInt(row[4]),
+                /* Type assertion. Consider this value as one of the possible
+                   values of MatchResult ('H', 'A', 'D')  */
+                row[5] as MatchResult,
+                row[6]
+            ]);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,18 @@ import { MatchResult } from "./MatchResult"
 const reader = new CsvFileReader("football.csv");
 reader.read();
 
-let manUnitedWins = 0;
+console.log(reader.data[0]);
 
-for (let match of reader.data) {
-    if (
-        (match[1] === "Man United" && match[5] === MatchResult.HomeWin) ||
-        (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
-    ) {
-        manUnitedWins++;
-    }
-}
 
-console.log(`Man United won ${manUnitedWins} games`);
+// let manUnitedWins = 0;
+
+// for (let match of reader.data) {
+//     if (
+//         (match[1] === "Man United" && match[5] === MatchResult.HomeWin) ||
+//         (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
+//     ) {
+//         manUnitedWins++;
+//     }
+// }
+
+// console.log(`Man United won ${manUnitedWins} games`);
